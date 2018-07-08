4ab919b7a6d7e0bcdc6a550bc7c18a2b20c0e948
miketonny <miketonny@gmail.com>
2018-07-09 10:46:16 +1200

    initial commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static(publicPath));
+// app.get('/', (req, res) => {
+//     res.sendfile(path.join(publicPath, '/index.html'));
+// });
+
+app.listen(port, () => {
+    console.log(`Started on port ${port}`);
+});

4b36ce3c95bf48890b5d57c7513cf59e4dab349d
miketonny <miketonny@gmail.com>
2018-07-10 14:42:15 +1200

    scroll update

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,19 @@
 var socket = io();
+
+function scrollToBottom () {
+    var messages = $('#messages');
+    var newMessage = messages.children('li:last-child');
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function () {
     console.log('Connected to server');
     // socket.emit('createEmail', {
@@ -34,6 +49,7 @@ socket.on('newMessage', function (msg) {
         createdAt: formattedTime
     });
     $('#messages').append(html);
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -50,6 +66,7 @@ socket.on('newLocationMessage', function (message) {
     // a.attr('href', message.url);
     // li.append(a);
     $('#messages').append(html);
+    scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function (e) {
